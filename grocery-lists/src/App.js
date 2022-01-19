9dde14a5562e7323d5ad207a87f0469149449eb2
BelenMaru <bleneee@gmail.com>
2022-01-18 19:21:41 -0500

    add states

--- grocery-lists/src/App.js
+++ grocery-lists/src/App.js
@@ -1,7 +1,10 @@
-import React, {useState, useEffect} from "react";
-import List from './List'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
+   const [list, setList] = useState("");
+    const [alert, setAlert] = useState({});
   return (
     <div className="App">
       <h2>Grocery Lists</h2>
