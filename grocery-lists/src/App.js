67d832f409fd5f96780dc684b0248d7ef20bd760
BelenMaru <bleneee@gmail.com>
2022-01-20 20:55:35 -0500

    add structure to the comp

--- grocery-lists/src/App.js
+++ grocery-lists/src/App.js
@@ -3,7 +3,10 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-   const [list, setList] = useState("");
+  const [name, setName] = useState("");
+  const [list, setList] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
     const [alert, setAlert] = useState({show:false, msg: '', type:''});
   return (
     <section className="section-center">
