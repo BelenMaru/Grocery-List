0eb1e45cda90bedc918dffe22e30b3a7ffa3bf64
BelenMaru <bleneee@gmail.com>
2022-01-22 22:23:21 -0500

    add and import delete button

--- grocery-lists/src/List.js
+++ grocery-lists/src/List.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {FaEdit} from "react-icons/fa";
+import {FaEdit, FaTrash} from "react-icons/fa";
 const List = ({ items }) => {
   return (
     <div className="grocery-list">
@@ -11,6 +11,9 @@ const List = ({ items }) => {
             <div className="btn-container">
               <button type="button" className="edit-btn">
                 <FaEdit />
+              </button>
+              <button type="button" className="delete-btn">
+                <FaTrash />
               </button>
             </div>
           </article>
