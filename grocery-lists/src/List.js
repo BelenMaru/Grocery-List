3d61ce369f5b8d903d4caf97a518c98955af5691
BelenMaru <bleneee@gmail.com>
2022-01-22 22:10:17 -0500

    map through item and return id

--- grocery-lists/src/List.js
+++ grocery-lists/src/List.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const List = () => {
-  return <h2>list component</h2>;
+const List = ({ item }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return <article key={id} className="grocery-item"></article>;
+      })}
+    </div>
+  );
 };
 
 export default List;
